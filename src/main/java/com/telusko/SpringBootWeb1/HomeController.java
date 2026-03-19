fb8e83c45a0de8ccd36a37948cfd65f0abb9f54b
chanbinchoi <classy9611@gmail.com>
2026-03-19 20:32:50 +0900

    Accepting Data the servlet way

---
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -0,0 +1,24 @@
+package com.telusko.SpringBootWeb1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("home method Called");
+        return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req) {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        
+        return "result.jsp";
+    }
+}
